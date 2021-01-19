a522d1694f850d2f74f6820364cc217704af111b
YeongG <yeonggil223@gmail.com>
2021-01-20 00:46:04 +0900

    [CREATE] USER ORM

---
+++ server/models/User.js
@@ -0,0 +1,38 @@
+const Sequelize = require("sequelize");
+
+module.exports = class User extends (
+  Sequelize.Model
+) {
+  static init(sequelize) {
+    return super.init(
+      {
+        email: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+        },
+        name: {
+          type: Sequelize.STRING(10),
+          allowNull: false,
+        },
+        password: {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+        },
+        profileImg: {
+          type: Sequelize.STRING(50),
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        modelName: "User",
+        tableName: "users",
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+  static associate(db) {
+    db.User.hasMany(db.Comment, { foreignKey: "commenterId", sourceKey: "id" });
+  }
+};
