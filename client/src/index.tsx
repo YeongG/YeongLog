8a4df9c3ec624234271aef4c1375196a3ed965b6
YeongG <yeonggil223@gmail.com>
2021-01-19 23:41:11 +0900

    [MODIFY] Provider와 store연결

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./modules";
 
-ReactDOM.render(<div>Test</div>, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
