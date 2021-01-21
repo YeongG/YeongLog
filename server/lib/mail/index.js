d8e1ad9b6c349c014045f2ec03cb7aaff7c9e94f
YeongG <yeonggil223@gmail.com>
2021-01-21 22:18:44 +0900

    [CREATE] mailHandler 라이브러리 개발

---
+++ server/lib/mail/index.js
@@ -0,0 +1,37 @@
+const nodemailer = require("nodemailer");
+
+class EmailHandler {
+  email;
+  transporter;
+  constructor(email, password) {
+    this.email = email;
+    this.transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmlail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: email,
+        pass: password,
+      },
+    });
+  }
+  sendEmail(options) {
+    const mailOptions = {
+      from: this.email,
+      to: options.toEmail,
+      subject: options.title,
+      text: options.content,
+    };
+    return new Promise((resolve, reject) => {
+      this.transporter.sendMail(mailOptions, function (err, info) {
+        if (err) {
+          reject(err);
+        }
+        resolve();
+      });
+    });
+  }
+}
+
+module.exports = EmailHandler;
