312e1a175fa940916beadd8e052b361ce6abba82
YeongG <yeonggil223@gmail.com>
2021-01-21 22:18:09 +0900

    [MODIFY] POST /sendemail

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -1,9 +1,43 @@
 const { Router } = require("express");
 const { User } = require("../../models");
 const { hashPassword } = require("../../lib/utils");
+const EmailHandler = require("../../lib/mail");
 const { checkBodyMiddleWare } = require("../../middleware");
+require("dotenv").config();
+
+const emailHandler = new EmailHandler(
+  process.env.NODEMAILER_USER,
+  process.env.NODEMAILER_PASS
+);
 
 const authRouter = Router();
+
+authRouter.post(
+  "/sendemail",
+  checkBodyMiddleWare(["email"]),
+  async (req, res) => {
+    const { email } = req.body;
+    const user = await User.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      const randomCode = Math.random().toString().substr(2, 6);
+      try {
+        await emailHandler.sendEmail({
+          title: "YeongLog 회원가입 인증 메일",
+          content: randomCode,
+          toEmail: email,
+        });
+        res.status(200).json({ message: "Sending Mail" });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Sending Mail Failed" });
+      }
+      return;
+    }
+  }
+);
 
 authRouter.post(
   "/register",
