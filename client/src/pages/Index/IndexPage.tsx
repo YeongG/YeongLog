966190785b8896d8e2ce83d7d3888d7b3a159235
YeongG <yeonggil223@gmail.com>
2021-01-21 22:19:47 +0900

    [MODIFY] 글 임시 더미데이터 추가

--- client/src/pages/Index/IndexPage.tsx
+++ client/src/pages/Index/IndexPage.tsx
@@ -11,6 +11,48 @@ const datas = [
     date: "2021년 1월 4일",
     commentCount: 10,
   },
+  {
+    title: "[  She++Korea ] 1기 멤버를 모집합니다. 💗🏳️‍🌈🌎",
+    introduce:
+      "테크기업에서 일하거나/ 일하고싶은 여성들이 모여서 서로 발전해가며 만들어가는 모임이예요",
+    date: "2021년 1월 4일",
+    commentCount: 10,
+  },
+  {
+    title: "[  She++Korea ] 1기 멤버를 모집합니다. 💗🏳️‍🌈🌎",
+    introduce:
+      "테크기업에서 일하거나/ 일하고싶은 여성들이 모여서 서로 발전해가며 만들어가는 모임이예요",
+    date: "2021년 1월 4일",
+    commentCount: 10,
+  },
+  {
+    title: "[  She++Korea ] 1기 멤버를 모집합니다. 💗🏳️‍🌈🌎",
+    introduce:
+      "테크기업에서 일하거나/ 일하고싶은 여성들이 모여서 서로 발전해가며 만들어가는 모임이예요",
+    date: "2021년 1월 4일",
+    commentCount: 10,
+  },
+  {
+    title: "[  She++Korea ] 1기 멤버를 모집합니다. 💗🏳️‍🌈🌎",
+    introduce:
+      "테크기업에서 일하거나/ 일하고싶은 여성들이 모여서 서로 발전해가며 만들어가는 모임이예요",
+    date: "2021년 1월 4일",
+    commentCount: 10,
+  },
+  {
+    title: "[  She++Korea ] 1기 멤버를 모집합니다. 💗🏳️‍🌈🌎",
+    introduce:
+      "테크기업에서 일하거나/ 일하고싶은 여성들이 모여서 서로 발전해가며 만들어가는 모임이예요",
+    date: "2021년 1월 4일",
+    commentCount: 10,
+  },
+  {
+    title: "[  She++Korea ] 1기 멤버를 모집합니다. 💗🏳️‍🌈🌎",
+    introduce:
+      "테크기업에서 일하거나/ 일하고싶은 여성들이 모여서 서로 발전해가며 만들어가는 모임이예요",
+    date: "2021년 1월 4일",
+    commentCount: 10,
+  },
 ];
 
 const IndexPage: FC<{}> = () => {
