e9ca7645aeb3dfdd425baa0e85cc23e8126696d4
YeongG <yeonggil223@gmail.com>
2021-01-22 20:41:42 +0900

    [MODIFY] 메일 발송 응답시간 개선

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { User } = require("../../models");
-const { hashPassword } = require("../../lib/utils");
+const { hashPassword, makeEmailJWT, decodeJwt } = require("../../lib/utils");
 const EmailHandler = require("../../lib/mail");
 const { checkBodyMiddleWare } = require("../../middleware");
 require("dotenv").config();
@@ -12,6 +12,16 @@ const emailHandler = new EmailHandler(
 
 const authRouter = Router();
 
+authRouter.get("/check-email", (req, res) => {
+  const token = req.headers.authorization;
+  const jwt = decodeJwt(token);
+  if (!jwt) {
+    res.status(409).json({ message: "Token X" });
+    return;
+  }
+  res.status(200).json({ message: "Token O", email: jwt.email });
+});
+
 authRouter.post(
   "/sendemail",
   checkBodyMiddleWare(["email"]),
@@ -22,18 +32,26 @@ authRouter.post(
     });
 
     if (!user) {
-      const randomCode = Math.random().toString().substr(2, 6);
-      try {
-        await emailHandler.sendEmail({
-          title: "YeongLog 회원가입 인증 메일",
-          content: randomCode,
-          toEmail: email,
-        });
-        res.status(200).json({ message: "Sending Mail" });
-      } catch (err) {
-        console.log(err);
-        res.status(500).json({ message: "Sending Mail Failed" });
-      }
+      const jwt = makeEmailJWT({ email });
+      emailHandler.sendEmail({
+        title: "YeongLog 회원가입 인증 메일",
+        html: `<!DOCTYPE html>
+            <html lang="en">
+              <head>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Document</title>  
+              </head>
+              <body>
+                <a href="http://localhost:8080/register?token=${jwt}" target="_blank">
+                  회원가입 하기
+                </a>
+              </body>
+            </html>
+            `,
+        toEmail: email,
+      });
+      res.status(200).json({ message: "Sending Mail" });
       return;
     }
   }
