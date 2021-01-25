a997c285cb53657b35e2ca202d0fd1ddbb0f52b1
YeongG <yeonggil223@gmail.com>
2021-01-25 14:47:48 +0900

    [CREATE] Register container

---
+++ client/src/containers/register/RegisterContainer.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import { RegisterPage } from "../../pages";
+
+const RegisterContainer: FC = () => {
+  return <RegisterPage />;
+};
+
+export default RegisterContainer;
