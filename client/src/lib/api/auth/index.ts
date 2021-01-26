f6f52a3bb98c23d61838fc9c203c4858482a7dba
YeongG <yeonggil223@gmail.com>
2021-01-26 23:13:15 +0900

    [MODIFY] api auth check-email/reqRegister 추가

--- client/src/lib/api/auth/index.ts
+++ client/src/lib/api/auth/index.ts
@@ -1,7 +1,23 @@
+import { AxiosResponse } from "axios";
+import { ReqRegister } from "../../payloads/auth";
 import { apiDefault } from "../../reqeust";
 
 export const sendEmail = (email: string) => {
   return apiDefault().post("/auth/sendemail", {
     email,
   });
+};
+
+export const checkEmail = (
+  token: string
+): Promise<AxiosResponse<{ email: string }>> => {
+  return apiDefault().get("/auth/check-email", {
+    headers: {
+      Authorization: token,
+    },
+  });
+};
+
+export const reqRegister = (registerData: ReqRegister) => {
+  return apiDefault().post("/auth/register", registerData);
 };
