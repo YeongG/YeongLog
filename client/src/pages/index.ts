1a1e3161c9cedfcb864ba0cdf2934732f61d2099
YeongG <yeonggil223@gmail.com>
2021-01-27 23:10:01 +0900

    [MODIFY] pages index.ts

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,2 +1,3 @@
 export { default as IndexPage } from "./Index/IndexPage";
 export { default as RegisterPage } from "./Register/RegisterPage";
+export { default as LoginPage } from "./Login/LoginPage";
