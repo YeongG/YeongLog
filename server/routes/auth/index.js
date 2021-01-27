b0d8ee09a4c23c0cd15ea6d38ccf1168efd58217
YeongG <yeonggil223@gmail.com>
2021-01-27 23:13:24 +0900

    [MODIFY] 이메일 체크, 메일 URL,  에러처리

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -1,6 +1,11 @@
 const { Router } = require("express");
 const { User } = require("../../models");
-const { hashPassword, makeEmailJWT, decodeJwt } = require("../../lib/utils");
+const {
+  hashPassword,
+  makeEmailJWT,
+  decodeJwt,
+  makeAccessJWT,
+} = require("../../lib/utils");
 const EmailHandler = require("../../lib/mail");
 const { checkBodyMiddleWare } = require("../../middleware");
 require("dotenv").config();
@@ -14,12 +19,13 @@ const authRouter = Router();
 
 authRouter.get("/check-email", (req, res) => {
   const token = req.headers.authorization;
-  const jwt = decodeJwt(token);
-  if (!jwt) {
+  try {
+    const jwt = decodeJwt(token);
+    res.status(200).json({ message: "Token O", email: jwt.email });
+  } catch (err) {
     res.status(409).json({ message: "Token X" });
     return;
   }
-  res.status(200).json({ message: "Token O", email: jwt.email });
 });
 
 authRouter.post(
@@ -43,7 +49,7 @@ authRouter.post(
                 <title>Document</title>  
               </head>
               <body>
-                <a href="http://localhost:8080/register?token=${jwt}" target="_blank">
+                <a href="http://121.184.45.242:8080/register?token=${jwt}" target="_blank">
                   회원가입 하기
                 </a>
               </body>
@@ -54,44 +60,48 @@ authRouter.post(
       res.status(200).json({ message: "Sending Mail" });
       return;
     }
+    const accessJWT = makeAccessJWT({ email });
+    emailHandler.sendEmail({
+      title: "YeongLog 로그인 메일",
+      html: `<!DOCTYPE html>
+          <html lang="en">
+            <head>
+              <meta charset="UTF-8" />
+              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+              <title>Document</title>  
+            </head>
+            <body>
+              <a href="http://121.184.45.242:8080/login?token=${accessJWT}" target="_blank">
+                로그인 하기
+              </a>
+            </body>
+          </html>
+          `,
+      toEmail: email,
+    });
+    res.status(200).json({ message: "Sending Mail" });
   }
 );
 
 authRouter.post(
   "/register",
-  checkBodyMiddleWare(["email", "name", "password"]),
+  checkBodyMiddleWare(["email", "name", "introduce"]),
   async (req, res) => {
-    const { email, name, password } = req.body;
+    const { email, name, introduce } = req.body;
     const user = await User.findOne({ where: { email } });
     if (user) {
       res.status(409).json({ message: "Already User" });
       return;
     }
 
-    User.create({
+    await User.create({
       email,
       name,
-      password: hashPassword(password),
+      introduce,
       profileImg: "",
     });
+
     res.status(200).json({ message: "Success User Create" });
-  }
-);
-
-authRouter.post(
-  "/login",
-  checkBodyMiddleWare(["email", "password"]),
-  async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({
-      where: { email, password: hashPassword(password) },
-    });
-    if (!user) {
-      res.status(404).json({ message: "Email, Password is not correct" });
-      return;
-    }
-
-    res.status(200).json(user);
   }
 );
 
