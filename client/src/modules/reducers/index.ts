2c2132302a8c71f8ab0ce10724380c00aceeeb31
YeongG <yeonggil223@gmail.com>
2021-01-29 23:45:12 +0900

    [MODIFY] redux reducer index.ts

--- client/src/modules/reducers/index.ts
+++ client/src/modules/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import headerReducer from "./header";
 import loginModalReducer from "./loginModal";
 
 const rootReducer = combineReducers({
   loginModal: loginModalReducer,
+  header: headerReducer,
 });
 
 export type Store = ReturnType<typeof rootReducer>;
