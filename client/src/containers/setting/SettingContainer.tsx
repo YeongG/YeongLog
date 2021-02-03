66023dd0303ebb218bdd17cf0858d8460afbcb71
YeongG <yeonggil223@gmail.com>
2021-02-03 23:44:15 +0900

    [CREATE] setting container

---
+++ client/src/containers/setting/SettingContainer.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import { SettingPage } from "../../pages";
+
+const SettingContainer: FC = () => {
+  return <SettingPage />;
+};
+
+export default SettingContainer;
