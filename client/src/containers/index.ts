a1f01b77e1ce32a38d0bcdf7c2a59cfea56e76df
YeongG <yeonggil223@gmail.com>
2021-02-03 23:44:01 +0900

    [MODIFY] container/page index.ts

--- client/src/containers/index.ts
+++ client/src/containers/index.ts
@@ -1,2 +1,3 @@
 export { default as RegisterContainer } from "./register/RegisterContainer";
 export { default as LoginContainer } from "./login/LoginContainer";
+export { default as SettingContainer } from "./setting/SettingContainer";
--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as IndexPage } from "./Index/IndexPage";
 export { default as RegisterPage } from "./Register/RegisterPage";
 export { default as LoginPage } from "./Login/LoginPage";
+export { default as SettingPage } from "./Setting/SettingPage";
