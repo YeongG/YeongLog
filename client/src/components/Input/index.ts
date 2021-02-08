338d100aebaf372b9a53adb6ac8887a628b731f3
YeongG <yeonggil223@gmail.com>
2021-02-08 23:05:08 +0900

    [MODIFY] component/input index.ts

--- client/src/components/Input/index.ts
+++ client/src/components/Input/index.ts
@@ -1,2 +1,3 @@
 export { default as FormInput } from "./FormInput/FormInput";
 export { default as EditInput } from "./EditInput/EditInput";
+export { default as IntroduceInputWrap } from "./IntroduceInputWrap/IntroduceInputWrap";
